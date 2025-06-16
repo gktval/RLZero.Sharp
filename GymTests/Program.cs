9869b5e39702961a7dd732f048ebec224ea20941
Travis <tyeik@gktechinc.com>
2025-06-16 08:22:15 -0600

    cleanup

--- GymTests/Program.cs
+++ GymTests/Program.cs
@@ -17,46 +17,12 @@ string yamlFile = "config-carracing.yaml";
 string yamContent = File.ReadAllText(yamlFile);
 
 var deserializer = new DeserializerBuilder()
-    .WithNamingConvention(UnderscoredNamingConvention.Instance)  // see height_in_inches in sample yml 
+    .WithNamingConvention(UnderscoredNamingConvention.Instance)  
     .Build();
 
 Config config = deserializer.Deserialize<Config>(yamContent);
 var env = new CarRacing(deviceType: DeviceType.CUDA, config.max_frames, config.image);
 var effZero = new EfficientZero.Main();
 effZero.Run(config, env, DeviceType.CUDA);
-
-
-//float k = (float)Math.Pow(3, .5);
-//var model = new DPW(0.25f, 0.2f, env.Observation, env, k);
-//bool isDone = false;
-//while (!isDone)
-//{
-//    model.Learn(500);
-//    var action = model.BestActionReward();
-//    var step = env.Step(action, env.Epoch);
-//    isDone = step.IsComplete;
-//    model.Forward(action, step.PostState);
-
-//    Console.WriteLine(string.Format("{0}", step.Reward));
-//}
-
-//isDone = false;
-//var actionList = env.GetActions();
-//env.Reset();
-//env.Render = true;
-//int epoch = 0;
-//float reward = 0;
-//while(!isDone)
-//{
-//    var step = env.Step(actionList[epoch], epoch);
-//    reward = step.Reward.Value;
-
-//    await Task.Delay(20);
-//    isDone = env.IsDone;
-//    epoch++;
-//}
-
-//if (reward > 0)
-//    Console.WriteLine("You Win!");
 
 Console.WriteLine("Done");
